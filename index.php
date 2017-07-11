50032123ea648f7cd587b8bd8db3be1292de40eb
paulrr88 <paul@Pauls-iMac.home>
2017-07-11 12:08:04 +0100

    Basic design implementation & home page

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
   <!-- Basic Page Needs
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <meta charset="utf-8">
-  <title>Home &mdash; Organisation Title</title>
-  <meta name="description" content="We meet at Dove Lodge, 49 Beach road, Littlehampton on alternate Sundays from 2pm to 5pm. Our objective is to advance the education and welfare of adults with learning difficulties, and to provide support for their families and carers, in order to provide a better standard of life for such people.">
+  <title>Home &mdash; Ferring Baptist Church</title>
+  <meta name="description" content="This is the official website for Ferring Baptist Church.">
 
   <!-- Mobile Specific Metas
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -32,195 +32,122 @@
   <!-- Google fonts
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
-  <!--<link href="https://fonts.googleapis.com/css?family=BenchNine" rel="stylesheet"><!--Bench Nine Google Font for headers-->
+  <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC" rel="stylesheet"> <!--Alegreya Sans SC Google Font for headers-->
 </head>
 
 <body>
 
-  <header id="banner" role="banner"><!--Start of header area containing website title, graphics and navigation etc-->
-    <nav id="top" role="navigation"><!--Start of top information area-->
-      <ul>
-        <li><a href="sitemap/">Sitemap</a></li>
-        <li><a href="folder/">Another link</a></li>
-      </ul>
-    </nav><!--End of top information area-->
+    <div class="container">
+        <header id="banner" role="banner" style="background-color: lightblue;"><!--Start of header area containing website title, graphics and navigation etc-->
+            <nav id="top" role="navigation"><!--Start of top information area-->
+                <ul>
+                    <li><a href="sitemap/">Sitemap</a></li>
+                    <li><a href="folder/">Another link</a></li>
+                </ul>
+            </nav><!--End of top information area-->
 
-    <h1 class="centre">Website Title</h1>
+            <h1 class="centre textbig textshadow-soft textwhite">Ferring Baptist Church</h1>
 
-    <h2 class="centre">Website Byline</h2>
+            <h5 class="centre textshadow-mid textwhite">Greystoke Road, Ferring, Worthing, West Sussex, BN12 5JL</h5>
 
-    <nav id="nav-main" role="navigation"><!--Main navigation start-->
-      <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-        <li>Link 4</li>
-        <li>Link 5</li>
-        <li>Link 6</li>
-        <li>Link 7</li>
-        <li>Link 8</li>
-        <li>Link 9</li>
-        <li>Link 10</li>
-      </ul>
-    </nav><!--Main navigation ends-->
-  </header><!--Header area ends-->
+            <nav id="nav-main" role="navigation"><!--Main navigation start-->
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Diary/events</li>
+                    <li>Holiday Clubs</li>
+                    <li>Newssheet</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav><!--Main navigation ends-->
+        </header><!--Header area ends-->
+
 
   <!-- Primary Page Layout
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
-    <main class="container" role="main" style="background: blue; color: white;"><!--Container for ALL content, everything inside this-->
-      <p>Container begins</p>
+        <main class="content" role="main" style="background: blue; color: white;"><!--Container for ALL content, everything inside this-->
+            <div class="hometop">
+                <div class="padding-3-right">
+                    <article class="four columns hometop-news u-pull-right" role="article">
+                        <h2>Important piece of news</h2>
 
-      <div class="">
-        <article role="article" style="background: orange;">
-          <h1 role="heading">Header for article (page content)</h1>
+                        <p>Blurb here about whatever the news is about!</p>
+                    </article>
+                    <article class="six columns" role="article">
+                        <p>Our life at FBC is founded on the scriptures and the 
+                            saving work of Jesus Christ, and our aim is to make Jesus 
+                            and his love the focus of everything we do. Our worship 
+                            blends older and newer styles in a way that seeks to be 
+                            encouraging, positive, and accessible to people of 
+                            different traditions and backgrounds. The 10:30 A.M. 
+                            service is definitely the larger of the two, with the 
+                            6:30 P.M. worship attractive to some because of its more 
+                            intimate atmosphere.</p>
+                        
+                        <p>Situated in Greystoke Road, we are just round the corner 
+                            from Church Lane and St. Andrew's C. of E. with whom 
+                            we work closely as we seek to represent Christ in Ferring.</p>
+                    </article>
+                </div><!--End of padding-3-right-->
+            </div><!--End of hometop-->
+        </main><!--End of content-->
 
-          <section style="background: yellow; color: black;">
-            <h2>Header for section within article</h2>
-
-            <p>Content for section</p>
-
-            <h3>Header 3</h3>
-
-            <h4>Header 4</h4>
-
-            <h5>Header 5</h5>
-
-            <h6>Header 6</h6>
-
-            <ul role="list">
-              <li role="listitem">Hey there. <code>This is a code snippet</code>
-                <ul role="list">
-                  <li role="listitem">Hey I'm embedded</li>
-                </ul>
-              </li>
-            </ul>
-
-            <table summary="">
-              <thead>
-                <tr>
-                  <th scope="col">Hey it's table time</td>
-                  <th scope="col">I'm the second column header</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>I'm the first cell in the table body gosh I have a lot of text</td>
-                  <td>I'm the second</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <figure class="width-50">
-              <img
-                alt="Photo of a bunny"
-                class="fluid-100"
-                role="img"
-                src="https://s-media-cache-ak0.pinimg.com/736x/0a/8b/91/0a8b91f0bfbbcbc65fb7d43cd9ff4c78.jpg"
-                title="Bunny Rabbit by Photographer"
-              >
-              <figcaption>Hey this is a caption for the above image</figcaption>
-            </figure>
-
-            <figure class="fluid-images-3 margin-4" style="background: magenta;">
-              <img
-                alt="Photo of a bunny"
-                class=""
-                role="img"
-                src="https://s-media-cache-ak0.pinimg.com/736x/0a/8b/91/0a8b91f0bfbbcbc65fb7d43cd9ff4c78.jpg"
-                title="Bunny Rabbit by Photographer"
-              >
-              <img
-                alt="Photo of a bunny"
-                class=""
-                role="img"
-                src="https://s-media-cache-ak0.pinimg.com/736x/0a/8b/91/0a8b91f0bfbbcbc65fb7d43cd9ff4c78.jpg"
-                title="Bunny Rabbit by Photographer"
-              >
-              <img
-                alt="Photo of a bunny"
-                class=""
-                role="img"
-                src="https://s-media-cache-ak0.pinimg.com/736x/0a/8b/91/0a8b91f0bfbbcbc65fb7d43cd9ff4c78.jpg"
-                title="Bunny Rabbit by Photographer"
-              >
-              <figcaption class="centre padding-3-left padding-3-right">Hey this is a caption for the above images plural. To add another image, just change it to fluid-xx depending on how many images (divided up into 100 or whatever width the figure container is). Obviously adjustments need to be made if padding and margins are needed</figcaption>
-            </figure>
-          </section>
-
-          <div class="four columns" style="background: green">
-            <p>Four columns</p>
-          </div>
-
-          <div class="three columns" style="background: green">
-            <p>Three columns</p>
-
-
-          </div>
-
-          <div class="four columns u-pull-right">
-            <section style="background: purple;">
-              <p>This area doesn't want to be part of the main article area! Behold u-pull-right.</p>
-            </section>
-          </div>
-        </article>
-      </div>
-
-      <p class="clear">End of container</p>
-    </main>
-
-    <footer>
-    <ul>
-      <li><a href="">Home</a></li>
-      <li><a href="events/">Events</a></li>
-      <li><a href="diary/">Diary</a></li>
-      <li><a href="news/">News</a></li>
-      <li><a href="videos/">Videos</a></li>
-      <li><a href="admin/">Admin</a></li>
-      <li><a href="findus/">Find Us</a></li>
-    </ul>
-
-    <br />
-
-    <div role="contentinfo"><!--Copyright info etc-->
-      <p><strong>&copy; <?php echo date('Y'); ?> Arun Sunshine Group | Designed by <a href="http://www.paulcomputerhelp.co.uk/">Paul Robinson</a></strong><br />
-
-      <br />
-      <br />
-
-      <a href="perch/">Perch Login</a></p>
-    </div>
-
-    <br />
-    <br />
-    <br />
-  </footer>
-
-  <script>
-  // DELETE THIS IF NOT NEEDED ON THIS PAGE
-  // fix for mobile table layout from http://sitesforprofit.com/responsive-tables-in-wordpress
-
-  var headertext = [];
-  var headers = document.querySelectorAll("thead");
-  var tablebody = document.querySelectorAll("tbody");
-
-  for (var i = 0; i < headers.length; i++) {
-    headertext[i]=[];
-    for (var j = 0, headrow; headrow = headers[i].rows[0].cells[j]; j++) {
-      var current = headrow;
-      headertext[i].push(current.textContent);
-      }
-  }
-
-  for (var h = 0, tbody; tbody = tablebody[h]; h++) {
-    for (var i = 0, row; row = tbody.rows[i]; i++) {
-      for (var j = 0, col; col = row.cells[j]; j++) {
-        col.setAttribute("data-th", headertext[h][j]);
+          <footer>
+              <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="events/">Events</a></li>
+                  <li><a href="diary/">Diary</a></li>
+                  <li><a href="news/">News</a></li>
+                  <li><a href="videos/">Videos</a></li>
+                  <li><a href="admin/">Admin</a></li>
+                  <li><a href="findus/">Find Us</a></li>
+              </ul>
+  
+              <br />
+  
+              <div role="contentinfo"><!--Copyright info etc-->
+                  <p><strong>&copy; <?php echo date('Y'); ?> Arun Sunshine Group | Designed by <a href="http://www.paulcomputerhelp.co.uk/">Paul Robinson</a></strong><br />
+  
+                  <br />
+                  <br />
+  
+                  <a href="perch/">Perch Login</a></p>
+              </div>
+  
+              <br />
+              <br />
+              <br />
+          </footer>
+      </div><!--END OF CONTAINER-->
+  
+    <script>
+    // DELETE THIS IF NOT NEEDED ON THIS PAGE
+    // fix for mobile table layout from http://sitesforprofit.com/responsive-tables-in-wordpress
+  
+    var headertext = [];
+    var headers = document.querySelectorAll("thead");
+    var tablebody = document.querySelectorAll("tbody");
+  
+    for (var i = 0; i < headers.length; i++) {
+      headertext[i]=[];
+      for (var j = 0, headrow; headrow = headers[i].rows[0].cells[j]; j++) {
+        var current = headrow;
+        headertext[i].push(current.textContent);
+        }
+    }
+  
+    for (var h = 0, tbody; tbody = tablebody[h]; h++) {
+      for (var i = 0, row; row = tbody.rows[i]; i++) {
+        for (var j = 0, col; col = row.cells[j]; j++) {
+          col.setAttribute("data-th", headertext[h][j]);
+        }
       }
     }
-  }
-</script>
-<!-- End Document
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-</body>
-</html>
+  </script>
+  
+  <!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  </body>
+  </html>
+  
