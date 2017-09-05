c6933f9fdf0187b4f1e31b035c063dfa328a297a
paulrr88 <paul@Pauls-iMac.home>
2017-09-05 13:34:58 +0100

    Home page Perch additions

--- index.php
+++ index.php
@@ -73,20 +73,7 @@
                         <?php perch_content('Important news section at top'); ?>
                     </article>
                 <article class="background-white75 border-top-15-orange boxshadow-softextra6 clear six columns margin-2-top" role="article">
-                    <?php /*perch_content('Intro blurb at top');*/ ?>
-                        <p>Welcome to the FBC website! Our life at Ferring Baptist Church is founded on the scriptures and the 
-                            saving work of Jesus Christ, and our aim is to make Jesus 
-                            and his love the focus of everything we do. Our worship 
-                            blends older and newer styles in a way that seeks to be 
-                            encouraging, positive, and accessible to people of 
-                            different traditions and backgrounds. The 10:30 A.M. 
-                            service is definitely the larger of the two, with the 
-                            6:30 P.M. worship attractive to some because of its more 
-                            intimate atmosphere.</p>
-
-                        <p>Situated in Greystoke Road, we are just round the corner 
-                            from Church Lane and St. Andrew's C. of E. with whom 
-                            we work closely as we seek to represent Christ in Ferring.</p>
+                    <?php perch_content('Intro blurb at top'); ?>
                 </article>
             </div><!--End of hometop-->
             
@@ -94,7 +81,7 @@
             
             <?php /*perch_content('Carousel of events');*/ ?>
             
-            <div class="carousel twelve columns">
+            <div class="carousel twelve columns" style="">
                 <div>
                     <div class="homecarousel homecarousel-1 twelve columns">
                         <div class="padding-3">
